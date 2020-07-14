4588dc6eceecf1f95640e85ce69c384b2890f8bb
mkasatkin <Flash20031966>
2020-07-14 10:42:26 +0400

    initial commit

--- test_363.py
+++ test_363.py
@@ -1,9 +1,7 @@
 import pytest
 from selenium import webdriver
-import math
 import time
-
-
+import math
 
 @pytest.fixture(scope="function")
 def browser():
@@ -13,24 +11,29 @@ def browser():
     print("\nquit browser..")
     browser.quit()
 
+@pytest.mark.parametrize('language', ["https://stepik.org/lesson/236895/step/1",
+                                      "https://stepik.org/lesson/236896/step/1",
+                                      "https://stepik.org/lesson/236897/step/1",
+                                      "https://stepik.org/lesson/236898/step/1",
+                                      "https://stepik.org/lesson/236899/step/1",
+                                      "https://stepik.org/lesson/236903/step/1",
+                                      "https://stepik.org/lesson/236904/step/1",
+                                      "https://stepik.org/lesson/236905/step/1"])
 
-class TestStepikUFO():
+def test_open_and_find_solution(browser, language):
+    link = language
+    browser.get(link)
+    browser.implicitly_wait(10)
+    st = browser.find_element_by_css_selector("textarea")
+    answer = math.log(int(time.time()))
+    st.send_keys(str(answer))
+    st2 = browser.find_element_by_xpath("//section/div/div[2]/div[4]/button")
+    st2.click()
+    browser.implicitly_wait(10)
+    st3 = browser.find_element_by_xpath("//section/div/div[1]/div/pre")
+    x = st3.text
+    assert x == "Correct!", "Fatal Error"
 
-    @pytest.mark.parametrize('language', ["https://stepik.org/lesson/236895/step/1",
-                                          "https://stepik.org/lesson/236896/step/1",
-                                          "https://stepik.org/lesson/236897/step/1",
-                                          "https://stepik.org/lesson/236898/step/1",
-                                          "https://stepik.org/lesson/236899/step/1",
-                                          "https://stepik.org/lesson/236903/step/1",
-                                          "https://stepik.org/lesson/236904/step/1",
-                                          "https://stepik.org/lesson/236905/step/1"])
-    def test_open_and_find_solution(self, browser, language):
-        browser.implicitly_wait(10)
-        link = {language}
-        browser.get(link)
-        st = browser.find_element_by_css_selector("textarea")
-        answer = math.log(int(time.time()))
-        st.send_keys(str(answer))
 
 
 
